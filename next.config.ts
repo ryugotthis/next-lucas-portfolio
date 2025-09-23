3d306373aaa5fa47e534e5eca18288ef9d686b88
Subin <ryucandoit6@gmail.com>
2025-09-23 17:23:45 +0900

    change ssr/isr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // SSG 빌드 설정
+  // output: 'export', // SSG 빌드 설정
   trailingSlash: true, // 정적 호스팅을 위한 trailing slash
   images: {
     unoptimized: true, // 정적 내보내기 시 이미지 최적화 비활성화
