580fc37ee21aadde6abed7eaa7679f63d127cab4
Subin <ryucandoit6@gmail.com>
2025-10-14 18:52:52 +0900

    add postcss autoprifierx

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 const config = {
   plugins: {
-    tailwindcss: {}, // 클래스 이름(bg-red-500 등)을 실제 CSS로 변환
-    autoprefixer: {}, // -webkit-, -moz- 같은 접두사를 자동으로 붙임
+    plugins: ['@tailwindcss/postcss', 'autoprefixer'],
+    // 클래스 이름(bg-red-500 등)을 실제 CSS로 변환
+    // -webkit-, -moz- 같은 접두사를 자동으로 붙임
   },
 };
 
