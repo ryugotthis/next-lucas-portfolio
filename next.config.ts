f9c01b3207adf019c491b90d5e4042e077ce98a6
Subin <ryucandoit6@gmail.com>
2025-10-20 21:15:59 +0900

    edict next.config file trailingSlach, uptimized setting

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // output: 'export', // SSG 빌드 설정
   trailingSlash: false, // 정적 호스팅을 위한 trailing slash
   images: {
-    unoptimized: true, // 정적 내보내기시에만 이미지 최적화 비활성화
+    unoptimized: false, // 정적 내보내기시에만 이미지 최적화 비활성화
   },
 };
 
